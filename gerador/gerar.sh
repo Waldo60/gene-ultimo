f7b02e1a512ae625b7d449e57dc8d05f2bff3074
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2022-02-27 14:51:54 -0400

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -7,7 +7,7 @@ BARRA="\033[1;36m---------------------------------------------------------------
 echo -e "$BARRA"
 cat << EOF
 
-           NEW KEY GENERATOR BY 8TH
+           NEW KEY GENERATOR BY prueba
            INSTALACOES: $(cat $IVAR)
            
 EOF
